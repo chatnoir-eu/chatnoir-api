ba3a0b2b4bdfdd91e5d65cd1afd37b79eb2a9494
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-20 01:36:28 +0100

    Fix unused re-export

--- chatnoir/util.py
+++ chatnoir/util.py
@@ -1,9 +1,9 @@
 try:
-    from functools import cached_property
+    from functools import cached_property  # noqa: F401
 except ImportError:
-    cached_property = property
+    cached_property = property  # noqa: F401
 
 try:
-    from typing import Literal
+    from typing import Literal  # noqa: F401
 except ImportError:
-    from typing_extensions import Literal
+    from typing_extensions import Literal  # noqa: F401
