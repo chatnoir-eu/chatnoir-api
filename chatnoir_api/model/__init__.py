086cbde69293e5115f76dddd0857c8da2eceebb3
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-24 12:40:44 +0100

    Add index prefix

--- chatnoir_api/model/__init__.py
+++ chatnoir_api/model/__init__.py
@@ -9,5 +9,18 @@ class Index(Enum):
     CommonCrawl1511 = "cc1511"
     CommonCrawl1704 = "cc1704"
 
+    @property
+    def prefix(self) -> str:
+        if self is Index.ClueWeb09:
+            return "clueweb09"
+        elif self is Index.ClueWeb12:
+            return "clueweb12"
+        elif self is Index.CommonCrawl1511:
+            return "commoncrawl"
+        elif self is Index.CommonCrawl1704:
+            return "commoncrawl"
+        else:
+            raise ValueError(f"Unknown Index enum: {self}")
+
 
 Slop = Literal[0, 1, 2]
