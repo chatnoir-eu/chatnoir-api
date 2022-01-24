865c49fa4d4d5e77112f552a1f2de1dac105dfce
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-24 12:59:41 +0100

    Update contents example

--- examples/html_contents.py
+++ examples/html_contents.py
@@ -1,7 +1,6 @@
 from uuid import UUID
 
-from chatnoir_api import html_contents
-from chatnoir_api.model import Index
+from chatnoir_api import html_contents, Index
 
 contents = html_contents(
     UUID("e635baa8-7341-596a-b3cf-b33c05954361"),
@@ -12,6 +11,19 @@ print(contents)
 plain_contents = html_contents(
     UUID("e635baa8-7341-596a-b3cf-b33c05954361"),
     Index.CommonCrawl1511,
+    plain=True,
+)
+print(plain_contents)
+
+contents = html_contents(
+    "clueweb09-en0051-90-00849",
+    Index.ClueWeb09,
+)
+print(contents)
+
+plain_contents = html_contents(
+    "clueweb09-en0051-90-00849",
+    Index.ClueWeb09,
     plain=True,
 )
 print(plain_contents)
