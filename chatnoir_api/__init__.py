3150308510232c50dd9037bae52b0c8c34410b55
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-24 13:31:03 +0100

    Re-export types

--- chatnoir_api/__init__.py
+++ chatnoir_api/__init__.py
@@ -1,9 +1,17 @@
 __version__ = "0.1.11"
 
 from chatnoir_api import html, model
-from chatnoir_api.model import highlight
+from chatnoir_api.model import highlight, result
 
 # Re-export child modules.
 html_contents = html.html_contents
 Index = model.Index
 Slop = model.Slop
+Highlight = highlight.Highlight
+HighlightedText = highlight.HighlightedText
+Result = result.Result
+Results = result.Results
+ResultsMeta = result.ResultsMeta
+SearchResult = result.SearchResult
+PhraseSearchResult = result.PhraseSearchResult
+MinimalPhraseSearchResult = result.MinimalPhraseSearchResult
