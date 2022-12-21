4e9028599543f59d04e54d2981fd037dbf59b8ed
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-12-21 02:37:57 +0100

    Add short UUID examples

--- examples/html_contents.py
+++ examples/html_contents.py
@@ -1,6 +1,19 @@
 from uuid import UUID
 
-from chatnoir_api import html_contents, Index
+from chatnoir_api import html_contents, Index, ShortUUID
+
+contents = html_contents(
+    "clueweb09-en0051-90-00849",
+    Index.ClueWeb09,
+)
+print(contents)
+
+plain_contents = html_contents(
+    "clueweb09-en0051-90-00849",
+    Index.ClueWeb09,
+    plain=True,
+)
+print(plain_contents)
 
 contents = html_contents(
     UUID("e635baa8-7341-596a-b3cf-b33c05954361"),
@@ -16,14 +29,16 @@ plain_contents = html_contents(
 print(plain_contents)
 
 contents = html_contents(
-    "clueweb09-en0051-90-00849",
-    Index.ClueWeb09,
+    ShortUUID("6svePe3PXteDeGPk1XqTLA"),
+    Index.ClueWeb22,
+    base_url="https://chatnoir.web.webis.de/"
 )
 print(contents)
 
 plain_contents = html_contents(
-    "clueweb09-en0051-90-00849",
-    Index.ClueWeb09,
+    ShortUUID("6svePe3PXteDeGPk1XqTLA"),
+    Index.ClueWeb22,
     plain=True,
+    base_url="https://chatnoir.web.webis.de/"
 )
 print(plain_contents)
