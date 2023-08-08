b799aba453567a871f52e1f89ae6be843b5b49b9
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2023-08-08 14:17:56 +0200

    Improve Chat API

---
+++ chatnoir_api/chat.py
@@ -0,0 +1,38 @@
+from typing import Literal
+from urllib.parse import urljoin
+
+from requests import post
+
+from chatnoir_api.constants import BASE_URL_CHAT
+
+ModelType = Literal[
+    "alpaca-en-7b",
+    "gpt2-base",
+    "gpt2-large",
+    "gpt2-xl",
+    "alpaca-en-7b-retrieve-clueweb22",
+    "alpaca-en-7b-prompt-retrieve-rewritten-clueweb22",
+    "gpt2-xl-rewrite-with-clueweb22",
+]
+
+
+def chat(
+        api_key: str,
+        input_sentence: str,
+        model: ModelType = "alpaca-en-7b"
+) -> str:
+    url = urljoin(BASE_URL_CHAT, f"generate/{model}")
+    data = {
+        "input_sentence": input_sentence,
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Api-Key": api_key
+    }
+    response = post(url, data=data, headers=headers)
+    response.raise_for_status()
+    response_json = response.json()
+    if "response" not in response_json:
+        raise ValueError(f"Invalid ChatNoir Chat response: {response_json}")
+    return response_json["response"]
--- chatnoir_api/constants.py
+++ chatnoir_api/constants.py
@@ -1,2 +1,3 @@
 BASE_URL = "https://chatnoir.eu/"
 BASE_URL_STAGING = "https://chatnoir.web.webis.de/"
+BASE_URL_CHAT = "https://chat.web.webis.de/"
