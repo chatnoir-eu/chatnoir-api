7daa695f5155cc6d5cc06ec06086449e44caa86e
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-10-27 21:16:53 +0100

    Update cache examples

--- examples/cache.py
+++ examples/cache.py
@@ -1,44 +1,27 @@
-from uuid import UUID
-
-from chatnoir_api import cache_contents, Index, ShortUUID
+from chatnoir_api import cache_contents, ShortUUID
 
 contents = cache_contents(
     "clueweb09-en0051-90-00849",
-    Index.ClueWeb09,
+    index="clueweb09",
 )
 print(contents)
 
 plain_contents = cache_contents(
     "clueweb09-en0051-90-00849",
-    Index.ClueWeb09,
-    plain=True,
-)
-print(plain_contents)
-
-contents = cache_contents(
-    UUID("e635baa8-7341-596a-b3cf-b33c05954361"),
-    Index.CommonCrawl1511,
-)
-print(contents)
-
-plain_contents = cache_contents(
-    UUID("e635baa8-7341-596a-b3cf-b33c05954361"),
-    Index.CommonCrawl1511,
+    index="clueweb09",
     plain=True,
 )
 print(plain_contents)
 
 contents = cache_contents(
     ShortUUID("MzOlTIayX9ub7c13GLPr_g"),
-    Index.ClueWeb22,
-    staging=True,
+    index="clueweb22/b",
 )
 print(contents)
 
 plain_contents = cache_contents(
     ShortUUID("MzOlTIayX9ub7c13GLPr_g"),
-    Index.ClueWeb22,
+    index="clueweb22/b",
     plain=True,
-    staging=True,
 )
 print(plain_contents)
