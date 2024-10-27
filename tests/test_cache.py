9f2983e09379d9d3992681eb5dc386b3c283b40e
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-10-27 21:18:16 +0100

    Improve cache tests

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,75 +1,118 @@
+from dataclasses import dataclass
 from uuid import UUID
+from pytest import fixture, skip
 
-from chatnoir_api import cache_contents, Index, ShortUUID
+from chatnoir_api import cache_contents, ShortUUID, Index
 
 
-def test_html_contents_trec_id() -> None:
+@dataclass(frozen=True)
+class _TestCase:
+    index: Index
+    uuid: UUID
+    trec_id: str | None
+    contains_raw: str
+    contains_plain: str
+
+
+@fixture(
+    scope="module",
+    params=[
+        _TestCase(
+            "clueweb09",
+            ShortUUID("6svePe3PXteDeGPk1XqTLA"),
+            "clueweb09-enwp03-01-23789",
+            """Test and Test-and-set""",
+            "Test and Test-and-set",
+        ),
+        _TestCase(
+            "clueweb12",
+            ShortUUID("f0dhEFj9VpixBIspw6xtVQ"),
+            "clueweb12-0200wb-93-16911",
+            """<title>Test, test, test</title>""",
+            "Test, test, test",
+        ),
+        _TestCase(
+            "clueweb22/b",
+            ShortUUID("UvPR6h5AWnCcnGoQSTQZTw"),
+            "clueweb22-en0044-06-02359",
+            """<title data-dcnode-id="184">Hello World!</title>""",
+            "Hello World!",
+        ),
+        _TestCase(
+            "msmarco-document-v2.1",
+            ShortUUID("RPcdaMiaUFqd4x4qZgkAjQ"),
+            "msmarco_v2.1_doc_18_2701526052",
+            """"title": "Test - Wikipedia",""",
+            "Test - Wikipedia",
+        ),
+        _TestCase(
+            "msmarco-passage-v2.1",
+            ShortUUID("JLNEtARbVQqPi1fxu4AbwQ"),
+            "msmarco_v2.1_doc_18_2701526052#0_3051069337",
+            """"title": "Test - Wikipedia",""",
+            "Test - Wikipedia",
+        ),
+        _TestCase(
+            "trec-tot/2024",
+            ShortUUID("sH2tKdt-WBKNwfAVfaWW8A"),
+            "23949652",
+            """"title": "Zack Test",""",
+            "Zack Test",
+        ),
+        # TODO: Add test for `vaswani`.
+    ],
+)
+def test_case(request) -> _TestCase:
+    return request.param
+
+
+def test_cache_contents_trec_id(test_case: _TestCase) -> None:
+    if test_case.trec_id is None:
+        skip("Test case does not have WARC ID.")
+
     contents = cache_contents(
-        "clueweb09-en0051-90-00849",
-        Index.ClueWeb09,
+        uuid_or_document_id=test_case.trec_id,
+        index=test_case.index,
     )
 
     assert contents is not None
     assert isinstance(contents, str)
-    assert "<title>Test, test, test</title>" in contents
+    assert test_case.contains_raw in contents
 
 
-def test_html_contents_plain_trec_id() -> None:
+def test_cache_contents_plain_trec_id(test_case: _TestCase) -> None:
+    if test_case.trec_id is None:
+        skip("Test case does not have WARC ID.")
+
     contents = cache_contents(
-        "clueweb09-en0051-90-00849",
-        Index.ClueWeb09,
-        plain=True
-    )
-
-    assert contents is not None
-    assert isinstance(contents, str)
-    assert "<title>Test, test, test</title>" in contents
-
-
-def test_html_contents_uuid() -> None:
-    contents = cache_contents(
-        UUID("e635baa8-7341-596a-b3cf-b33c05954361"),
-        Index.CommonCrawl1511,
-    )
-
-    assert contents is not None
-    assert isinstance(contents, str)
-    assert "<title>hello world</title>" in contents
-
-
-def test_html_contents_plain_uuid() -> None:
-    contents = cache_contents(
-        UUID("e635baa8-7341-596a-b3cf-b33c05954361"),
-        Index.CommonCrawl1511,
+        uuid_or_document_id=test_case.trec_id,
+        index=test_case.index,
         plain=True,
     )
 
     assert contents is not None
     assert isinstance(contents, str)
-    assert "<title>hello world</title>" in contents
+    assert test_case.contains_plain in contents
 
 
-def test_html_contents_short_uuid() -> None:
+def test_cache_contents_uuid(test_case: _TestCase) -> None:
     contents = cache_contents(
-        ShortUUID("f6J0lMPmVfWs19jJNQkHKA"),
-        Index.ClueWeb22,
-        staging=True,
+        uuid_or_document_id=test_case.uuid,
+        index=test_case.index,
     )
 
     assert contents is not None
     assert isinstance(contents, str)
-    assert "<title data-dcnode-id=\"1108\">" \
-           "Hello World | Codecademy</title>" in contents
+    assert test_case.contains_raw in contents
 
 
-def test_html_contents_plain_short_uuid() -> None:
+def test_cache_contents_plain_uuid(test_case: _TestCase) -> None:
     contents = cache_contents(
-        ShortUUID("f6J0lMPmVfWs19jJNQkHKA"),
-        Index.ClueWeb22,
+        uuid_or_document_id=test_case.uuid,
+        index=test_case.index,
         plain=True,
-        staging=True,
     )
 
     assert contents is not None
     assert isinstance(contents, str)
-    assert "Hello World" in contents
+    assert test_case.contains_plain in contents
