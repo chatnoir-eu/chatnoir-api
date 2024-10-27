40ab4ee63710b1af74a6c652acf4b2df4180c938
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-10-27 21:10:56 +0100

    Migrate index enum to literal

--- chatnoir_api/model/__init__.py
+++ chatnoir_api/model/__init__.py
@@ -1,37 +1,75 @@
 from base64 import urlsafe_b64decode
-from enum import Enum
-from typing_extensions import Literal, TypeAlias
+from typing import Literal, TypeAlias
 from uuid import UUID
 
 
+Index: TypeAlias = Literal[
+    "clueweb09",
+    "clueweb12",
+    "clueweb22/b",
+    "msmarco-document-v2.1",
+    "msmarco-passage-v2.1",
+    "msmarco-passage-v2.1",
+    "trec-tot/2024",
+    # "vaswani",
+]
 
-class Index(Enum):
-    ClueWeb09 = "cw09"
-    ClueWeb12 = "cw12"
-    ClueWeb22 = "cw22"
-    CommonCrawl1511 = "cc1511"
-    CommonCrawl1704 = "cc1704"
-    MSMarcoV21 = "msmarco-v2.1"
-    MSMarcoV21Segmented = "msmarco-v2.1-segmented"
 
-    @property
-    def prefix(self) -> str:
-        if self is Index.ClueWeb09:
-            return "clueweb09"
-        elif self is Index.ClueWeb12:
-            return "clueweb12"
-        elif self is Index.ClueWeb22:
-            return "clueweb22"
-        elif self is Index.CommonCrawl1511:
-            return "commoncrawl"
-        elif self is Index.CommonCrawl1704:
-            return "commoncrawl"
-        elif self is Index.MSMarcoV21:
-            return "msmarco-v2.1-document"
-        elif self is Index.MSMarcoV21Segmented:
-            return "msmarco-v2.1-document-segmented"
-        else:
-            raise ValueError(f"Unknown Index enum: {self}")
+def parse_index(index_id: str) -> Index:
+    if index_id == "cw09":
+        return "clueweb09"
+    elif index_id == "cw12":
+        return "clueweb12"
+    elif index_id == "cw22":
+        return "clueweb22/b"
+    elif index_id == "msmarco-v2.1":
+        return "msmarco-document-v2.1"
+    elif index_id == "msmarco-v2.1-segmented":
+        return "msmarco-passage-v2.1"
+    elif index_id == "trec-tot-2024":
+        return "trec-tot/2024"
+    # elif index_id == "vaswani":
+    #     return "vaswani"
+    else:
+        raise ValueError(f"Unknown index ID: {index_id}")
+
+
+def index_id(index: Index) -> str:
+    if index == "clueweb09":
+        return "cw09"
+    elif index == "clueweb12":
+        return "cw12"
+    elif index == "clueweb22/b":
+        return "cw22"
+    elif index == "msmarco-document-v2.1":
+        return "msmarco-v2.1"
+    elif index == "msmarco-passage-v2.1":
+        return "msmarco-v2.1-segmented"
+    elif index == "trec-tot/2024":
+        return "trec-tot-2024"
+    # elif index == "vaswani":
+    #     return "vaswani"
+    else:
+        raise ValueError(f"Unknown index: {index}")
+
+
+def index_prefix(index: Index) -> str:
+    if index == "clueweb09":
+        return "clueweb09"
+    elif index == "clueweb12":
+        return "clueweb12"
+    elif index == "clueweb22/b":
+        return "clueweb22"
+    elif index == "msmarco-document-v2.1":
+        return "msmarco-v2.1-document"
+    elif index == "msmarco-passage-v2.1":
+        return "msmarco-v2.1-document-segmented"
+    elif index == "trec-tot/2024":
+        return "trec-tot-2024-document"
+    # elif index == "vaswani":
+    #     return "vaswani"
+    else:
+        raise ValueError(f"Unknown index: {index}")
 
 
 Slop = Literal[0, 1, 2]
