6c008f2a1864931f06f42c488107ba986fc52ae4
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-10-27 21:15:47 +0100

    Fix cache API

--- chatnoir_api/cache.py
+++ chatnoir_api/cache.py
@@ -1,42 +1,35 @@
 from typing import Union
 from urllib.parse import urljoin
 from uuid import UUID, uuid5, NAMESPACE_URL
-import json
 
 from requests import get, Response
 
-from chatnoir_api.constants import BASE_URL, BASE_URL_STAGING
-from chatnoir_api.model import Index
+from chatnoir_api.constants import BASE_URL
+from chatnoir_api.defaults import DEFAULT_TIMEOUT
+from chatnoir_api.model import Index, index_id, index_prefix
 
 
 def cache_contents(
-        uuid_or_document_id: Union[UUID, str],
-        index: Index,
-        plain: bool = False,
-        staging: bool = False,
+    uuid_or_document_id: Union[UUID, str],
+    index: Index,
+    plain: bool = False,
+    timeout: int = DEFAULT_TIMEOUT,
 ) -> str:
     uuid: UUID
     if isinstance(uuid_or_document_id, str):
-        uuid = uuid5(NAMESPACE_URL, f"{index.prefix}:{uuid_or_document_id}")
+        uuid = uuid5(NAMESPACE_URL, f"{index_prefix(index)}:{uuid_or_document_id}")
     else:
         uuid = uuid_or_document_id
 
-    base_url = BASE_URL_STAGING if staging else BASE_URL
     response: Response = get(
-        urljoin(base_url, "cache"),
+        urljoin(BASE_URL, "cache"),
         params={
             "uuid": str(uuid),
-            "index": index.value,
-            "raw": True,
-            "plain": plain,
-        }
+            "index": index_id(index),
+            "raw": "true",
+            "plain": "true" if plain else "false",
+        },
+        timeout=timeout,
     )
     response.raise_for_status()
-
-    if index in (Index.MSMarcoV21, Index.MSMarcoV21Segmented):
-        ret = json.loads(response.text)
-        if uuid_or_document_id != ret['docid']:
-            raise ValueError(f'Document Id is not as expected. Expected "{uuid_or_document_id}" but have "{ret["docid"]}".')
-        return ret
-
     return response.text
