556b3e6d21b2e9a3254e996db73f7c5ecd158bfc
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-10-27 21:13:32 +0100

    Consolidate API models

--- chatnoir_api/v1/model.py
+++ chatnoir_api/v1/model.py
@@ -1,118 +1,69 @@
-from abc import ABC
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import List, Optional, Set
+from typing import Optional, Sequence, Set
 from uuid import UUID
 
 from dataclasses_json import config, DataClassJsonMixin
 
-from chatnoir_api.model import Index, Slop, decode_uuid
+from chatnoir_api.model import Index, Slop, decode_uuid, index_id, parse_index
 from chatnoir_api.model.highlight import HighlightedText
-from chatnoir_api.model.result import Meta, Result, ExtendedMeta, MetaIndex, \
-    MinimalResult, MinimalResultStaging, ExplainedMinimalResult, Explanation, \
-    ExplainedMinimalResultStaging, ExplainedResult, ResultStaging, \
-    ExplainedResultStaging, ResultsMixin
+from chatnoir_api.model.result import (
+    Meta,
+    Result,
+    ExtendedMeta,
+    MetaIndex,
+    MinimalResult,
+    ExplainedMinimalResult,
+    Explanation,
+    ExplainedResult,
+    ResultsMixin,
+)
 
 
 @dataclass(frozen=True)
 class Request(DataClassJsonMixin):
     apikey: str
     query: str
-    index: Set[Index] = field(metadata=config(
-        encoder=lambda indices: {index.value for index in indices}
-    ))
-    start: Optional[int] = field(metadata=config(
-        field_name="from"
-    ))
+    index: Set[Index] = field(
+        metadata=config(encoder=lambda indices: {index_id(index) for index in indices})
+    )
+    start: Optional[int] = field(metadata=config(field_name="from"))
     size: Optional[int]
     explain: bool
     minimal: bool
-
-
-@dataclass(frozen=True)
-class PhraseRequest(Request, DataClassJsonMixin, ABC):
-    slop: Optional[Slop]
-
-
-@dataclass(frozen=True)
-class RequestStaging(Request, DataClassJsonMixin, ABC):
     extended_meta: bool
 
 
 @dataclass(frozen=True)
-class PhraseRequestStaging(
-    RequestStaging, DataClassJsonMixin, ABC
-):
+class PhraseRequest(Request, DataClassJsonMixin):
     slop: Optional[Slop]
 
 
 @dataclass(frozen=True)
 class MinimalResultResponse(MinimalResult, DataClassJsonMixin):
     score: float
-    uuid: UUID = field(metadata=config(
-        decoder=decode_uuid
-    ))
+    uuid: UUID = field(metadata=config(decoder=decode_uuid))
     target_uri: str
-    snippet: HighlightedText = field(metadata=config(
-        decoder=HighlightedText
-    ))
+    snippet: HighlightedText = field(metadata=config(decoder=HighlightedText))
+    index: Index = field(metadata=config(decoder=parse_index))
+    title: HighlightedText = field(
+        metadata=config(
+            encoder=str,
+            decoder=HighlightedText,
+        )
+    )
 
 
 @dataclass(frozen=True)
 class ExplanationResponse(Explanation, DataClassJsonMixin):
     value: float
     description: str
-    details: List["Explanation"]
+    details: Sequence["Explanation"]
 
 
 @dataclass(frozen=True)
 class ExplainedMinimalResultResponse(
     MinimalResultResponse, ExplainedMinimalResult, DataClassJsonMixin
-):
-    explanation: ExplanationResponse
-
-
-@dataclass(frozen=True)
-class ResultResponse(MinimalResultResponse, Result, DataClassJsonMixin):
-    index: Index = field(metadata=config(
-        encoder=str,
-        decoder=Index
-    ))
-    trec_id: Optional[str]
-    target_hostname: str
-    page_rank: Optional[float]
-    spam_rank: Optional[float]
-    title: HighlightedText = field(metadata=config(
-        encoder=str,
-        decoder=HighlightedText
-    ))
-
-
-@dataclass(frozen=True)
-class ExplainedResultResponse(
-    ResultResponse, ExplainedMinimalResultResponse, ExplainedResult,
-    DataClassJsonMixin
-):
-    explanation: ExplanationResponse
-
-
-@dataclass(frozen=True)
-class MinimalResultResponseStaging(
-    MinimalResultResponse, MinimalResultStaging, DataClassJsonMixin
-):
-    index: Index = field(metadata=config(
-        decoder=Index
-    ))
-    title: HighlightedText = field(metadata=config(
-        encoder=str,
-        decoder=HighlightedText,
-    ))
-
-
-@dataclass(frozen=True)
-class ExplainedMinimalResultResponseStaging(
-    MinimalResultResponseStaging, ExplainedMinimalResultResponse,
-    ExplainedMinimalResultStaging, DataClassJsonMixin
 ):
     explanation: ExplanationResponse
 
@@ -124,49 +75,51 @@ def _decode_datetime(value: Optional[str]) -> Optional[datetime]:
 
 
 @dataclass(frozen=True)
-class ResultResponseStaging(
-    MinimalResultResponseStaging, ResultResponse, ResultStaging,
-    DataClassJsonMixin
-):
+class ResultResponse(MinimalResultResponse, Result, DataClassJsonMixin):
+    index: Index = field(metadata=config(decoder=parse_index))
+    trec_id: Optional[str]
+    target_hostname: str
+    page_rank: Optional[float]
+    spam_rank: Optional[float]
+    title: HighlightedText = field(metadata=config(decoder=HighlightedText))
     warc_id: Optional[str]
-    cache_uri: Optional[str]
-    crawl_date: Optional[datetime] = field(metadata=config(
-        decoder=_decode_datetime
-    ))
+    cache_uri: str
+    crawl_date: Optional[datetime] = field(metadata=config(decoder=_decode_datetime))
     content_type: Optional[str]
     language: str = field(metadata=config(field_name="lang"))
 
 
 @dataclass(frozen=True)
-class ExplainedResultResponseStaging(
-    ResultResponseStaging, ExplainedResultResponse, ExplainedResultStaging,
-    DataClassJsonMixin
+class ExplainedResultResponse(
+    ResultResponse, ExplainedMinimalResultResponse, ExplainedResult, DataClassJsonMixin
 ):
     explanation: ExplanationResponse
 
 
 @dataclass(frozen=True)
 class MetaResponse(Meta, DataClassJsonMixin):
-    indices: Set[Index] = field(metadata=config(
-        decoder=lambda indices: {Index(index) for index in indices}
-    ))
+    indices: Set[Index] = field(
+        metadata=config(decoder=lambda ids: {parse_index(id) for id in ids})
+    )
     query_time: int
     total_results: int
 
 
 @dataclass(frozen=True)
 class MetaIndexResponse(MetaIndex, DataClassJsonMixin):
-    index: Index = field(metadata=config(
-        decoder=Index,
-        field_name="id",
-    ))
+    index: Index = field(
+        metadata=config(
+            decoder=parse_index,
+            field_name="id",
+        )
+    )
     name: str
     selected: bool
 
 
 @dataclass(frozen=True)
 class ExtendedMetaResponse(MetaResponse, ExtendedMeta, DataClassJsonMixin):
-    indices: Set[MetaIndexResponse]
+    indices: Set[MetaIndexResponse]  # type: ignore
     explain: bool
     max_page: int
     page_size: int
@@ -177,109 +130,64 @@ class ExtendedMetaResponse(MetaResponse, ExtendedMeta, DataClassJsonMixin):
 
 
 @dataclass(frozen=True)
-class MinimalResponse(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
+class MinimalSearchResponse(
+    DataClassJsonMixin, ResultsMixin[MetaResponse, MinimalResultResponse]
 ):
-    meta: MetaResponse
-    results: List[MinimalResultResponse]
+    _meta: MetaResponse = field(metadata=config(field_name="meta"))
+    _results: Sequence[MinimalResultResponse] = field(metadata=config(field_name="results"))
 
 
 @dataclass(frozen=True)
-class ExplainedMinimalResponse(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
+class ExplainedMinimalSearchResponse(
+    DataClassJsonMixin, ResultsMixin[MetaResponse, MinimalResultResponse]
 ):
-    meta: MetaResponse
-    results: List[ExplainedMinimalResultResponse]
+    _meta: MetaResponse = field(metadata=config(field_name="meta"))
+    _results: Sequence[ExplainedMinimalResultResponse] = field(metadata=config(field_name="results"))
 
 
 @dataclass(frozen=True)
-class Response(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
+class SearchResponse(
+    DataClassJsonMixin, ResultsMixin[MetaResponse, MinimalResultResponse]
 ):
-    meta: MetaResponse
-    results: List[ResultResponse]
+    _meta: MetaResponse = field(metadata=config(field_name="meta"))
+    _results: Sequence[ResultResponse] = field(metadata=config(field_name="results"))
 
 
 @dataclass(frozen=True)
-class ExplainedResponse(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
+class ExplainedSearchResponse(
+    DataClassJsonMixin, ResultsMixin[MetaResponse, MinimalResultResponse]
 ):
-    meta: MetaResponse
-    results: List[ExplainedResultResponse]
+    _meta: MetaResponse = field(metadata=config(field_name="meta"))
+    _results: Sequence[ExplainedResultResponse] = field(metadata=config(field_name="results"))
 
 
 @dataclass(frozen=True)
-class MinimalResponseStaging(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
+class ExtendedMetaMinimalSearchResponse(
+    DataClassJsonMixin, ResultsMixin[MetaResponse, MinimalResultResponse]
 ):
-    meta: MetaResponse
-    results: List[MinimalResultResponseStaging]
+    _meta: ExtendedMetaResponse = field(metadata=config(field_name="meta"))
+    _results: Sequence[MinimalResultResponse] = field(metadata=config(field_name="results"))
 
 
 @dataclass(frozen=True)
-class ExplainedMinimalResponseStaging(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
-
+class ExplainedExtendedMetaMinimalSearchResponse(
+    DataClassJsonMixin, ResultsMixin[MetaResponse, MinimalResultResponse]
 ):
-    meta: MetaResponse
-    results: List[ExplainedMinimalResultResponseStaging]
+    _meta: ExtendedMetaResponse = field(metadata=config(field_name="meta"))
+    _results: Sequence[ExplainedMinimalResultResponse] = field(metadata=config(field_name="results"))
 
 
 @dataclass(frozen=True)
-class ResponseStaging(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
+class ExtendedMetaSearchResponse(
+    DataClassJsonMixin, ResultsMixin[MetaResponse, MinimalResultResponse]
 ):
-    meta: MetaResponse
-    results: List[ResultResponseStaging]
+    _meta: ExtendedMetaResponse = field(metadata=config(field_name="meta"))
+    _results: Sequence[ResultResponse] = field(metadata=config(field_name="results"))
 
 
 @dataclass(frozen=True)
-class ExplainedResponseStaging(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
+class ExplainedExtendedMetaSearchResponse(
+    DataClassJsonMixin, ResultsMixin[MetaResponse, MinimalResultResponse]
 ):
-    meta: MetaResponse
-    results: List[ExplainedResultResponseStaging]
-
-
-@dataclass(frozen=True)
-class ExtendedMetaMinimalResponseStaging(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
-):
-    meta: ExtendedMetaResponse
-    results: List[MinimalResultResponseStaging]
-
-
-@dataclass(frozen=True)
-class ExplainedExtendedMetaMinimalResponseStaging(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
-):
-    meta: ExtendedMetaResponse
-    results: List[ExplainedMinimalResultResponseStaging]
-
-
-@dataclass(frozen=True)
-class ExtendedMetaResponseStaging(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
-):
-    meta: ExtendedMetaResponse
-    results: List[ResultResponseStaging]
-
-
-@dataclass(frozen=True)
-class ExplainedExtendedMetaResponseStaging(
-    DataClassJsonMixin,
-    ResultsMixin[MetaResponse, MinimalResultResponse]
-):
-    meta: ExtendedMetaResponse
-    results: List[ExplainedResultResponseStaging]
+    _meta: ExtendedMetaResponse = field(metadata=config(field_name="meta"))
+    _results: Sequence[ExplainedResultResponse] = field(metadata=config(field_name="results"))
