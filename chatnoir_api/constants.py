70dd29f194b64ce354febca641f47af7417e834a
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2025-05-16 18:15:35 +0200

    Update API endpoint and default index

--- chatnoir_api/constants.py
+++ chatnoir_api/constants.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://chatnoir.web.webis.de/"
-BASE_URL_WEBCONTENT = "https://chatnoir-webcontent.web.webis.de/"
+BASE_URL = "https://www.chatnoir.eu/"
+BASE_URL_WEBCONTENT = "https://chatnoir-webcontent.chatnoir.eu/"
 BASE_URL_CHAT = "https://chat.web.webis.de/"
 BASE_URL_CHAT_SOCKET = "wss://chatsocket.web.webis.de"
--- chatnoir_api/defaults.py
+++ chatnoir_api/defaults.py
@@ -5,8 +5,7 @@ DEFAULT_START: int = 0
 DEFAULT_SIZE: int = 10
 DEFAULT_SLOP: Slop = 0
 DEFAULT_INDEX: Set[Index] = {
-    "clueweb09",
-    "clueweb12",
+    "clueweb22/b",
 }
 DEFAULT_MINIMAL: bool = False
 DEFAULT_EXPLAIN: bool = False
@@ -14,6 +13,6 @@ DEFAULT_EXTENDED_META: bool = False
 DEFAULT_RETRIES: int = 5
 DEFAULT_BACKOFF_SECONDS: int = 1
 DEFAULT_TIMEOUT: int = 60
-# Note: This public API key has a small request budget. If you want to use ChatNoir more extensively, please request an API key at https://chatnoir.web.webis.de/apikey/
+# Note: This public API key has a small request budget. If you want to use ChatNoir more extensively, please request an API key at https://www.chatnoir.eu/apikey/
 DEFAULT_API_KEY = "LTmnNLQeQvBlNjwWeuNxz1vdya3HpSzN"
 DEFAULT_SEARCH_METHOD: SearchMethod = "default"
